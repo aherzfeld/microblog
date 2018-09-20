c7b55fc6f9021c6142ceaa9b4c600eb902782b60
aherzfeld <herzfeld.andrew@gmail.com>
2018-09-20 16:14:47 +1000

    Finished Part3: Web Forms

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask  # imports Flask object from flask package
+from config import Config  # imports Config class from config.py
 
 app = Flask(__name__)  # creates app object as instance of class Flask
+app.config.from_object(Config)
 
 # this app package is defined by the app directory and the __init__.py script
 # imported below app instantiation to avoid circular imports
