79ecb42f3190ad9a3d719736e23cfbb0f8b1a8a0
aherzfeld <herzfeld.andrew@gmail.com>
2018-10-12 11:54:17 +1100

    added config.py to git

---
+++ config.py
@@ -0,0 +1,30 @@
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    # database
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # email
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    # boolean to enable encrypted connections
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    # username and password optional
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    # list of admin email addresses
+    ADMINS = ['herzfeld.andrew@gmail.com']
+    # pagination
+    POSTS_PER_PAGE = 25
+    # translation support for Babel
+    LANGUAGES = ['en', 'es']
+    # Microsoft translate text api
+    MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
